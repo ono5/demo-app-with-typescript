dd1f7fee907ca637d39fc8adb841ffdd6b314709
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-08-14 07:05:08 +0900

    feat: get form value by addEventListner

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
     element: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    mandayInputElement: HTMLInputElement
 
     constructor() {
         // template要素への参照
@@ -15,7 +18,25 @@ class ProjectInput {
         // Domに追加したい具体的にHTML要素を取得する
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = 'user-input'
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+        this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement
+
+        this.configure()
         this.attach()
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault()
+        console.log(this.titleInputElement.value)
+        console.log(this.descriptionInputElement.value)
+        console.log(this.mandayInputElement.value)
+    }
+
+    // EventListnerの登録を行う
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
     }
 
     //　要素を追加する
