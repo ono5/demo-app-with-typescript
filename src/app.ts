040b4c6a9a1b37f4279d610500bb2abc84864476
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-08-19 07:18:15 +0900

    implement Component class

--- src/app.ts
+++ src/app.ts
@@ -133,6 +133,38 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     return adjDescriptor
 }
 
+// Component Class
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    templateElement: HTMLTemplateElement
+    hostElement: T
+    element: U
+
+    constructor(
+        templateId: string,
+        hostElementId: string,
+        insertAtStart: boolean,
+        newElementId?: string) {
+        this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement
+        this.hostElement = document.getElementById(hostElementId) as T
+        const importedNode = document.importNode(this.templateElement.content, true)
+        this.element = importedNode.firstElementChild as U
+        if (newElementId) {
+            this.element.id = newElementId
+        }
+        this.attach(insertAtStart)
+    }
+
+    // abstractメソッドは具体的な実装はしない
+    // 継承先のクラスでこのメソッドを実装することを強制する
+    abstract configure(): void
+    abstract renderContent(): void
+
+    private attach(insertAtBeginning: boolean) {
+        this.hostElement.insertAdjacentElement(
+            insertAtBeginning ? 'afterbegin' : 'beforeend', this.element)
+    }
+}
+
 // ProjectList Class
 // プロジェクトのリストを表示する
 class ProjectList {
